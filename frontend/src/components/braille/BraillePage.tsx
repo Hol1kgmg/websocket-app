ea930c1e2b137eb8a98675abe9facc296f58c434
Holikgmg <abdevs.horisho@icloud.com>
2026-01-06 20:44:36 +0900

    fix:ページ下部にスペースを追加

--- frontend/src/components/braille/BraillePage.tsx
+++ frontend/src/components/braille/BraillePage.tsx
@@ -11,7 +11,7 @@ export const BraillePage = () => {
 
   return (
     <>
-      <div className="flex flex-col items-start">
+      <div className="flex flex-col items-start pb-10">
         <div className="flex flex-col mb-6 p-4">
           <TypographyH1>点字テキスト検索フォーム</TypographyH1>
           <TypographyP>全256個の点字文字をビジュアル検索（U+2800 〜 U+28FF）</TypographyP>
